ae8a76372e8adebf51df92b2ca4c80d3faeca4a8
Ryan Delbango <Ryan.Delbango@gmail.com>
2022-10-27 16:39:29 -0400

    Add: Views directory, Home view init, sample asset

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div>
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+<style lang="">
+    
+</style>
