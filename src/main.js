652fdcdd6961a875a20cdb7ade1cbfce4e3fb54f
Ryan Delbango <Ryan.Delbango@gmail.com>
2022-10-28 17:10:40 -0400

    Add: nav template, bootstrap set up

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue } from 'bootstrap-vue'
 
-Vue.config.productionTip = false
+import './app.scss'
+
+Vue.use(BootstrapVue)
 
 new Vue({
   render: h => h(App),
